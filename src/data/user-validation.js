88036ef4976668a7f3088c77a1d7d1e65a0ed00e
oxiadenine <sga.samuel@gmail.com>
2025-09-01 19:10:22 +0200

    Add user data validation

---
+++ src/data/user-validation.js
@@ -0,0 +1,47 @@
+import { UserError } from "./user-error";
+
+export class UserValidation {
+  errors = { nickname: [], password: []};
+  
+  get hasErrors() {
+    return this.errors.nickname.length > 0 || this.errors.password.length > 0;
+  }
+
+  throwIfErrors(message) {
+    if (this.hasErrors) {
+      throw new UserError(message, {
+        cause: { code: UserError.Code.validity, errors: this.errors }
+      });
+    }
+  }
+
+  static validate(user, messages) {
+    const validation = new UserValidation();
+
+    const nickname = user.nickname;
+
+    if (!nickname) {
+      validation.errors.nickname.push({ type: "empty", message: messages.nickname.empty });
+    }
+    if (nickname.length < 3 || nickname.length > 16) {
+      validation.errors.nickname.push({ type: "length", message: messages.nickname.length });
+    }
+    if (!/^[a-z0-9]+$/i.test(nickname)) {
+      validation.errors.nickname.push({ type: "format", message: messages.nickname.format });
+    }
+
+    const password = user.password;
+
+    if (!password) {
+      validation.errors.password.push({ type: "empty", message: messages.password.empty });
+    }
+    if (password.length < 12 || password.length > 32) {
+      validation.errors.password.push({ type: "length", message: messages.password.length });
+    }
+    if (!/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/.test(password)) {
+      validation.errors.password.push({ type: "format", message: messages.password.format });
+    }
+
+    return validation;
+  }
+}
